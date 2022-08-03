a034db47af875e8fb874d9dfab25e89beaa91704
Amelie Pira <amelie.pira@gmail.com>
2022-08-03 15:55:50 +0100

    amended uri for server start

--- src/index.js
+++ src/index.js
@@ -17,8 +17,9 @@ app.use(routes);
 const init = async () => {
   try {
     const DB_NAME = process.env.DB_NAME;
+    //getting an error with `mongodb://localhost:27017/${DB_NAME} and server doesn't start, so replaced it with `mongodb://0.0.0.0:27017/${DB_NAME}`
     const MONGODB_URI =
-      process.env.MONGODB_URI || `mongodb://localhost:27017/${DB_NAME}`;
+      process.env.MONGODB_URI || `mongodb://0.0.0.0:27017/${DB_NAME}`;
 
     const options = {
       useNewUrlParser: true,
