6fe043f298d2a78440541f897172cb73d696e709
Amelie Pira <amelie.pira@gmail.com>
2022-08-05 13:31:25 +0100

    added seedUsers function

--- src/seeds/users.js
+++ src/seeds/users.js
@@ -1,21 +1,38 @@
 const { User } = require("../models");
+const { faker } = require("@faker-js/faker");
 
-const users = [
-  {
-    username: "bob451",
-    email: "bobsmith@email.com",
-  },
-  {
-    username: "alice345",
-    email: "alicesmith@email.com",
-  },
-  {
-    username: "max610",
-    email: "maxblack@email.com",
-  },
-];
+// const users = [
+//   {
+//     username: "bob451",
+//     email: "bobsmith@email.com",
+//   },
+//   {
+//     username: "alice345",
+//     email: "alicesmith@email.com",
+//   },
+//   {
+//     username: "max610",
+//     email: "maxblack@email.com",
+//   },
+// ];
+
+const prepareUsersData = () => {
+  const users = [];
+
+  //create a number of users
+  for (let i = 0; i < 20; i += 1) {
+    const username = faker.internet.username();
+    const email = faker.internet.exampleEmail();
+    const user = { username, email };
+    users.push(user);
+  }
+
+  // return users array
+  return users;
+};
 
 const seedUsers = async () => {
+  const users = await prepareUsersData();
   const promises = users.map((user) => User.create(user));
 
   await Promise.all(promises);
