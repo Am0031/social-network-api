dd61f6c1fc79d6b037fa74c1ba7f553236543f90
Amelie Pira <amelie.pira@gmail.com>
2022-08-05 15:35:02 +0100

    added notes

--- src/models/Reaction.js
+++ src/models/Reaction.js
@@ -2,7 +2,7 @@ const { Schema, model, Types } = require("mongoose");
 const { format } = require("date-fns");
 
 const reactionSchema = {
-  //does not override the _id but is set to be the same value as _id when creating the reaction
+  //does not override the _id, it is an additional field created so to make sure it doesn't have 2 id values, it is set to be the same value as _id when creating the reaction
   reactionId: {
     type: Schema.Types.ObjectId,
     default: () => new Types.ObjectId(),
@@ -29,7 +29,7 @@ const reactionSchema = {
     get: (val) => format(new Date(val), "yyyy-MM-dd'T'HH:mm:ss"),
   },
 };
-
+//_id: false -> removed because not allowed - must always be an _id with actual name of _id when creating a document (unless using sharding? but no idea how to do that)
 const schema = new Schema(reactionSchema, {
   toJSON: { getters: true },
 });
