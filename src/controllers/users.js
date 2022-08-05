d443149ff24342e6ec8c1b62fe1e494444756dd0
Amelie Pira <amelie.pira@gmail.com>
2022-08-05 16:54:04 +0100

    added controller function getUsers and getUserById

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,16 +1,36 @@
-const getAllUsers = (req, res) => {
-  return res.json({ message: "getting users" });
+const { User } = require("../models");
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}).populate("friends");
+
+    return res.json({ data: users });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get all users | ${error.message}`);
+  }
 };
-const getUserById = (req, res) => {
-  return res.json({ message: "getting user by ID" });
+const getUserById = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ success: false });
+    }
+
+    return res.json({ data: user });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get user by id | ${error.message}`);
+  }
 };
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
   return res.json({ message: "creating user" });
 };
-const updateUserById = (req, res) => {
+const updateUserById = async (req, res) => {
   return res.json({ message: "updating user" });
 };
-const deleteUserById = (req, res) => {
+const deleteUserById = async (req, res) => {
   return res.json({ message: "deleting user" });
 };
 
