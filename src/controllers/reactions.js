9efc85bbf1293b3558f19df5a6af411f41e3c7a5
Amelie Pira <amelie.pira@gmail.com>
2022-08-08 20:38:01 +0100

    added controller function createReaction

--- src/controllers/reactions.js
+++ src/controllers/reactions.js
@@ -1,5 +1,45 @@
+const { Reaction, Thought } = require("../models");
+
 const createReactionForThought = async (req, res) => {
-  return res.json({ message: "creating Reaction for Thought" });
+  try {
+    const { thoughtId } = req.params;
+    const { reactionBody, username } = req.body;
+
+    const createdAt = new Date();
+
+    const newReactionData = { username, reactionBody, createdAt };
+
+    const reaction = await Reaction.create(newReactionData);
+
+    if (!reaction) {
+      return res.status(404).json({ message: `Reaction not created` });
+    }
+
+    const reactionId = reaction._id;
+
+    const reactionIdToUpdate = await Reaction.findByIdAndUpdate(reactionId, {
+      $set: {
+        reactionId: reactionId,
+      },
+    });
+
+    const thoughtToUpdate = await Thought.findOneAndUpdate(
+      { _id: thoughtId },
+      {
+        $push: {
+          reactions: reactionId,
+        },
+      }
+    );
+
+    return res.status(201).json({
+      message: "Reaction successfully created",
+      thoughtId: thoughtId,
+      reactionId: reactionId,
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create reaction | ${error.message}`);
+  }
 };
 
 const deleteReactionForThought = async (req, res) => {
