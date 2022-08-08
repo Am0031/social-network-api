4df50b59be883cef53c2a7548b2f90d7e1bcda78
Amelie Pira <amelie.pira@gmail.com>
2022-08-08 17:58:08 +0100

    added controller function createFriend

--- src/controllers/friends.js
+++ src/controllers/friends.js
@@ -1,8 +1,41 @@
-const createFriendForUser = (req, res) => {
-  return res.json({ message: "creating friend for user" });
+const { User } = require("../models");
+
+const createFriendForUser = async (req, res) => {
+  try {
+    const { userId } =  req.params;
+    const { friendId } = req.body;
+
+    const firstStep = await User.findOneAndUpdate(
+      {_id: userId},
+      {
+        $push: {
+          friends: friendId,
+        },
+      }
+    );
+
+    if (!firstStep) {
+      return res.status(404).json({ message: `Friendship not created` });
+    }
+
+    const secondStep = await User.findOneAndUpdate(
+      {_id: friendId},
+      {
+        $push: {
+          friends: userId,
+        },
+      }
+    );
+
+    return res.status(201).json({
+      message: "Friendship successfully created"
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create friendship | ${error.message}`);
+  }
 };
 
-const deleteFriendForUser = (req, res) => {
+const deleteFriendForUser = async (req, res) => {
   return res.json({ message: "deleting friend for user" });
 };
 
