28dc704129550104f798190600af2657c8dda580
Amelie Pira <amelie.pira@gmail.com>
2022-08-08 14:36:46 +0100

    added controller function getThoughtById

--- src/controllers/thoughts.js
+++ src/controllers/thoughts.js
@@ -6,11 +6,25 @@ const getAllThoughts = async (req, res) => {
 
     return res.json({ data: thoughts });
   } catch (error) {
-    console.log(`[ERROR]: Failed to get all users | ${error.message}`);
+    console.log(`[ERROR]: Failed to get all thoughts | ${error.message}`);
   }
 };
 const getThoughtById = async (req, res) => {
-  return res.json({ message: "getting Thought by ID" });
+  try {
+    const { thoughtId } = req.params;
+
+    const thought = await Thought.findById(thoughtId);
+
+    if (!thought) {
+      return res
+        .status(404)
+        .json({ message: `Thought with id ${thoughtId} not found` });
+    }
+
+    return res.json({ data: thought });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get thought by id | ${error.message}`);
+  }
 };
 const createThought = async (req, res) => {
   return res.json({ message: "creating Thought" });
