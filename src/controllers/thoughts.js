b8fbc18855cafa55056641e7448958a4f75a6e24
Amelie Pira <amelie.pira@gmail.com>
2022-08-08 14:34:25 +0100

    added controller function getThoughts

--- src/controllers/thoughts.js
+++ src/controllers/thoughts.js
@@ -1,16 +1,24 @@
-const getAllThoughts = (req, res) => {
-  return res.json({ message: "getting Thoughts" });
+const { Thought } = require("../models");
+
+const getAllThoughts = async (req, res) => {
+  try {
+    const thoughts = await Thought.find({});
+
+    return res.json({ data: thoughts });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get all users | ${error.message}`);
+  }
 };
-const getThoughtById = (req, res) => {
+const getThoughtById = async (req, res) => {
   return res.json({ message: "getting Thought by ID" });
 };
-const createThought = (req, res) => {
+const createThought = async (req, res) => {
   return res.json({ message: "creating Thought" });
 };
-const updateThoughtById = (req, res) => {
+const updateThoughtById = async (req, res) => {
   return res.json({ message: "updating Thought" });
 };
-const deleteThoughtById = (req, res) => {
+const deleteThoughtById = async (req, res) => {
   return res.json({ message: "deleting Thought" });
 };
 
